6ea28266806244c2001c2851af6489e8f7a0d707
hashkitty <hashkittie@gmail.com>
2018-04-19 15:08:37 +0300

    ck client export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.Logger = require('./providers/log/logger');
 exports.Database = require('./providers/database/database');
+exports.CryptoKittiesClient = require('./providers/ethereum/cryptokitties-client');
 exports.DatabaseSync = require('./workers/synchronization/database-sync');
-
